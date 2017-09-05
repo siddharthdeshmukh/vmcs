fa66e10d31a6ef5c7c86c6b17a9e85fba5286e32
sharmamedha <sharmamedha93@gmail.com>
2017-09-05 13:25:17 +0800

    Enhancement:02-Show denomination quantity and its amount

--- setenv.bat
+++ setenv.bat
@@ -1,5 +1,5 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_121
-set ANT_HOME=..\apache-ant-1.6.5
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131
+set ANT_HOME=C:\Program Files\Java\apache-ant-1.6.5
 set JUNIT_HOME=..\junit3.8.2
 
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
